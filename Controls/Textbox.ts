8e20dc92cac47238187a16ae2350e2e842cf645c
sisatia <sisatia@microsoft.com>
2020-07-09 15:43:48 +0530

    Add init and inputValidity to Textbox control

--- Controls/Textbox.ts
+++ Controls/Textbox.ts
@@ -13,11 +13,27 @@ export class Textbox extends BaseControl{
         super(name, selector, page);
     }
 
+    public async init() {
+        this.textbox = await super.init();
+    }
+
+    /*
+     * Provide input to the text field
+     */
     public async input(value: string) {
         await Utilities.inputTextHelper(this.selector, value, this.page, true);
 
         //Update logging functionality
         logger.info(this.name + " textbox Update!");
+    }
+    /*
+     * Provide input to the text field
+     */
+
+    public async isInputValid() {
+        await Utilities.delay(500);
+        let validity: string = await this.page.evaluate((element:any) => element.getAttribute('data-validatable-control-validation-state'), this.textbox);
+        return validity == "valid" ? true : false;
     }
 
 }
