88c6d439355d82516d1522b34989342cc4c53704
sisatia <sisatia@microsoft.com>
2020-08-07 12:30:24 +0530

    Add logger for yamlParsing

--- YamlParser/main.ts
+++ YamlParser/main.ts
@@ -1,8 +1,29 @@
 import { YamlParser } from "./YamlParser"
+import * as winston from "winston"
+import _ from "lodash"
+
+const globalVariables = _.pick(global, ['logger']);
 
 async function main() {
+    global.logger = winston.createLogger({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          winston.format.align(),
+          winston.format.printf(
+            info => `${info.level}: ${info.message}`
+          )
+        ),
+        transports: [
+          new (winston.transports.File)({
+            filename: 'UITesting.log',
+            level: 'info'
+          })
+        ],
+      });
+      
     var parser: YamlParser = new YamlParser();
     parser.RenderTestTemplates();
 }
 
-main();
+main(); 
