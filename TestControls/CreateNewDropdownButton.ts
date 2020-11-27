63073ddc4f51991a77f3185ea37a73dcae75890c
sisatia <sisatia@microsoft.com>
2020-11-27 18:52:49 +0530

    Update Test CreateNewDropDownButton control name

--- TestControls/CreateNewDropdownButton.ts
+++ TestControls/CreateNewDropdownButton.ts
@@ -6,19 +6,19 @@ export class TestCreateNewDropdownButton extends TestBaseControl {
     /**
      * Dropdown object
      */
-    public createNew: CreateNewDropdownButton;
+    public createNewDropdownButton: CreateNewDropdownButton;
 
     constructor(name: string, selector: string, page: any) {
         super(name, selector, page);
-        this.createNew = new CreateNewDropdownButton(name, selector, page);
+        this.createNewDropdownButton = new CreateNewDropdownButton(name, selector, page);
     }
 
     public async init() {
-        await this.createNew.init();
+        await this.createNewDropdownButton.init();
     }
 
     public async testExists(){
-        await super.testExists(this.createNew);
+        await super.testExists(this.createNewDropdownButton);
     }
 
 }
