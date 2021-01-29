502f5e7948103f1b4c2cabfd06eac508e2ebb083
sisatia <sisatia@microsoft.com>
2021-01-29 12:31:51 +0530

    Add injectjquery parameter in .env

--- Main/main.ts
+++ Main/main.ts
@@ -1,6 +1,8 @@
 /// <reference path="../bootstrap.ts" />
 import 'mocha';
 import {YamlParser} from "../YamlParser/YamlParser"
+// import dotenv from "dotenv"
+// dotenv.config()
 
 describe('Load Starting page', async function () {
     //var page: any;
@@ -11,9 +13,11 @@ describe('Load Starting page', async function () {
         global.logger.info("INITIATE TESTING");
         global.page = await browser.newPage();
         let gotoPage: string = parser.gotoPage;
-        const jquery = await page.evaluate(() => window.fetch('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js').then((res) => res.text()));
         await global.page.goto(gotoPage);
-        await page.evaluate(jquery);
+        if (process.env.INJECTJQUERY == "1") {
+            const jquery = await global.page.evaluate(() => window.fetch('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js').then((res) => res.text()));
+            await page.evaluate(jquery);
+        }
     });
 
     it('Initiate Testing', async function() {
